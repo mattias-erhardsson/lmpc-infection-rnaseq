63fcd8b2e2b4bbf9ff563ab017f31e276f168e2e
Mattias Erhardsson <mattias.erhardsson@gmail.com>
2023-08-03 07:42:33 +0200

    Adapted script for 12 cores in order to run SetRank with 10 cores

--- lmpc-infection-rnaseq-analysis.sh
+++ lmpc-infection-rnaseq-analysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -A naiss2023-22-759
 #SBATCH -p core
-#SBATCH -n 20
-#SBATCH -o /crex/proj/naiss2023-22-759/%x.output
-#SBATCH -e /crex/proj/naiss2023-22-759/%x.output
+#SBATCH -n 12
+#SBATCH -o /crex/proj/naiss2023-22-759/lmpc-infection-rnaseq/%x.output
+#SBATCH -e /crex/proj/naiss2023-22-759/lmpc-infection-rnaseq/%x.output
 #SBATCH -t 4-00:00:00
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
