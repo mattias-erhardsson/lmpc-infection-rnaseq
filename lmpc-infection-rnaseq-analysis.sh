89ae1a53114a6ddbc8b84c4111c6bac1081386e6
mattias-erhardsson <mattias.erhardsson@gmail.com>
2023-08-28 11:48:18 +0200

    Fat UPPMAX node

--- lmpc-infection-rnaseq-analysis.sh
+++ lmpc-infection-rnaseq-analysis.sh
@@ -2,6 +2,7 @@
 #SBATCH -A naiss2023-22-759
 #SBATCH -p node
 #SBATCH -N 1
+#SBATCH -C mem256GB
 #SBATCH -o /crex/proj/naiss2023-22-759/lmpc-infection-rnaseq/%x.output
 #SBATCH -e /crex/proj/naiss2023-22-759/lmpc-infection-rnaseq/%x.output
 #SBATCH -t 2-00:00:00
